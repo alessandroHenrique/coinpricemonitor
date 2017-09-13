377074ce8eed7ae860a5ee2d1f27dd54f6f5e25e
Alessandro <alessandro.henrique@labcodes.com.br>
2017-09-13 15:05:41 -0300

    Rename project folder

--- bitcoinmonitor/routing.py
+++
@@ -1,9 +0,0 @@
-from channels import route
-
-from dashboard import consumers
-
-
-channel_routing = [
-    route('websocket.connect', consumers.ws_connect),
-    route('websocket.disconnect', consumers.ws_disconnect),
-]
