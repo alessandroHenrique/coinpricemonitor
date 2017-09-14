4dd13a8b031a57f6290fb43dc6daa5082041658a
Alessandro <alessandro.henrique@labcodes.com.br>
2017-09-14 09:39:12 -0300

    Add ws_receive consumer with channel_session_user decorator

--- dashboard/consumers.py
+++ dashboard/consumers.py
@@ -1,11 +1,14 @@
+import json
+
 from channels import Group
-from channels.auth import channel_session_user, channel_session_user_from_http
+from channels.auth import channel_session_user
 
 
-@channel_session_user_from_http
+@channel_session_user
 def ws_connect(message):
-
     Group('btc-price').add(message.reply_channel)
+
+    message.channel_session['coin-group'] = 'btc-price'
 
     message.reply_channel.send({
         'accept': True
@@ -13,5 +16,24 @@ def ws_connect(message):
 
 
 @channel_session_user
+def ws_receive(message):
+    data = json.loads(message.content.get('text'))
+
+    if data.get('coin') == 'litecoin':
+        Group('ltc-price').add(message.reply_channel)
+        Group('btc-price').discard(message.reply_channel)
+
+        message.channel_session['coin-group'] = 'ltc-price'
+
+    elif data.get('coin') == 'bitcoin':
+        Group('btc-price').add(message.reply_channel)
+        Group('ltc-price').discard(message.reply_channel)
+
+        message.channel_session['coin-group'] = 'btc-price'
+
+
+@channel_session_user
 def ws_disconnect(message):
-    Group('btc-price').discard(message.reply_channel)
+    user_group = message.channel_session['coin-group']
+
+    Group(user_group).discard(message.reply_channel)
