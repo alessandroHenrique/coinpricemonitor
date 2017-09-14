7c2a9288cd3fbe56cc0698296fce927fb52f3c3c
Alessandro <alessandro.henrique@labcodes.com.br>
2017-09-14 09:41:08 -0300

    Add logic to toggle the price between litecoin and bitcoin

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -1,7 +1,9 @@
 var wsUri = "ws://localhost:8000/ws/";
+var websocket;
+
 
 function setupWebSocket() {
-  var websocket = new WebSocket(wsUri);
+  websocket = new WebSocket(wsUri);
   websocket.onopen = function(evt) { onOpen(evt) };
   websocket.onmessage = function(evt) { onMessage(evt) };
 }
@@ -11,8 +13,35 @@ function onOpen (evt) {
 }
 
 function onMessage (evt) {
-  var spanWithPrice = document.getElementsByClassName('bitcoin-price')[0];
-  spanWithPrice.innerHTML = ' $' + String(JSON.parse(evt.data).last_price);
+  var data = JSON.parse(evt.data);
+
+  if (data.BTC) {
+    $('.bitcoin-price')[0].innerHTML = ' $' + String(data.BTC);
+  } else {
+    $('.litecoin-price')[0].innerHTML = ' $' + String(data.LTC);
+  }
 }
+
+$(".change-coin-js").click(function (e) {
+  if ($(".wrapper-bitcoin").css("display") == "none") {
+    $(".wrapper-bitcoin").css("display", "block");
+    $(".wrapper-litecoin").css("display", "none");
+
+    $(".change-coin-js")[0].innerHTML = "Change to litecoin";
+
+    websocket.send(JSON.stringify({
+      coin: 'bitcoin'
+    }))
+  } else if ($(".wrapper-litecoin").css("display") == "none") {
+    $(".wrapper-litecoin").css("display", "block");
+    $(".wrapper-bitcoin").css("display", "none");
+
+    $(".change-coin-js")[0].innerHTML = "Change to bitcoin";
+
+    websocket.send(JSON.stringify({
+      coin: 'litecoin'
+    }))
+  }
+});
 
 window.addEventListener("load", setupWebSocket, false);
